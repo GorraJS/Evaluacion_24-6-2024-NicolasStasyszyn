bf77fcccba6fe147d1b10aa89772be89c275bd34
Gorra.js <gorrahabla1234@gmail.com>
2024-06-24 15:47:24 -0300

    Ejercicio 2

--- src/components/Ejercicio2.jsx
+++ src/components/Ejercicio2.jsx
@@ -1,35 +1,41 @@
-import React from "react"
-import { useState } from "react"
+import React from "react";
+import { useState } from "react";
 
 export default function Ejercicio2() {
-  
-  const [texto, setTexto] = useState('')
-  const [num, setNum] = useState(0)
+  const [texto, setTexto] = useState("");
+  const [num, setNum] = useState();
 
   const handleChangeTexto = (event) => {
-    setTexto(event.target.value)
-  }
-  const handleChangeNum = () => {
-    setNum(num)
-  }
-  const Submit = () => {
+    setTexto(event.target.value);
+  };
+  const handleChangeNum = (event) => {
+    setNum(event.target.value);
+  };
 
-  }
+  const Submit = () => {
+    if (num < 18) {
+      window.alert(texto + " tiene " + num + " años y es menor de edad");
+    } else {
+      window.alert(texto + " tiene " + num + " años y es mayor de edad");
+    }
+  };
 
   return (
     <div>
       <h2>Ejercicio 2: Formulario</h2>
       <form>
         <div>
-          <input type="text" value={texto} onChange={handleChangeTexto}/>
+          <input type="text" value={texto} onChange={handleChangeTexto} />
         </div>
         <div>
-          <input type="number" value={num} onChange={handleChangeNum}/> 
+          <input type="numeric" value={num} onChange={handleChangeNum} />
         </div>
         <div>
-          <button type="submit" onClick={Submit}/>
+          <button type="submit" onClick={Submit}>
+            enviar
+          </button>
         </div>
       </form>
     </div>
-  )
+  );
 }
